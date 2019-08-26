67be75f0b29848e348bebb286307301b144a30e4
cambiph <incubusattax@gmail.com>
2019-08-26 08:35:07 +0200

    Update

--- didm/batchUpdateFiles.js
+++ didm/batchUpdateFiles.js
@@ -2,7 +2,7 @@ const replace = require('replace-in-file');
 const options = {
     files: '/Users/philippe/Documents/localdev/webcomponent-vl-ui-*/package.json',
     from: /"prepare": "npm run prepare:util"/g,
-    to: '"prepare": "npm update --dev && npm run prepare:util"',
+    to: '"prepare": "npm update --dev vl-ui-util && npm run prepare:util"',
     countMatches: true,
     dry: false
 };
--- didm/executeCommands.js
+++ didm/executeCommands.js
@@ -1,10 +1,17 @@
 const folderUtils = require('../utils/folderUtils');
 const shell = require('../utils/shellUtils');
 
+const branchName            = 'UIG-167';
+const deleteLocalBranches   = 'git branch | grep -v \"master\" | xargs git branch -D';
+const createNewBranch       = 'git stash && git master && ' + deleteLocalBranches + ' && git pull && git checkout -b' + branchName;
+
+const commitMessage         = 'Perform npm update --dev vl-ui-util on prepare';
+const pushChanges           = 'git add . && git commit -m \"' + commitMessage + '\" && git push --set-upstream origin ' + branchName;
+
 async function run() {
     const result = await folderUtils.getDirs();
     result.forEach((entry) => {
-        shell.execute('cd ' + entry + ' && git master && git pull && git checkout -b UIG-212');
+        shell.execute('cd ' + entry + ' && ' + pushChanges);
     });
 }
 
