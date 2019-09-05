a7e077a73262567f227adaad3d800fb969f0d4ab
cambiph <incubusattax@gmail.com>
2019-09-05 09:55:24 +0200

    Add script to update all components and create maintenance branch

---
+++ didm/updateAllAndCreateMaintenanceBranch.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+
+WORKINGDIR="/Users/philippe/Documents/localdev/"
+BRANCHNAME="maintenance"
+
+DIRS=`ls $WORKINGDIR | grep "webcomponent-vl-ui-*"`
+
+for dir in $DIRS; do
+    pushd $WORKINGDIR$dir \
+    && echo "====================================================" \
+    && echo " Working on $dir" \
+    && echo "====================================================" \
+    && git master \
+    && git stash \
+    && git pull \
+    && checkIfMaintenanceBranchExists $BRANCHNAME \
+    && git checkout -b $BRANCHNAME \
+    && echo "====================================================" \
+    && echo " Done for $dir" \
+    && echo "====================================================" \
+    && popd
+done
