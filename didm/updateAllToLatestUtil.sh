ace5159ce46c9b6087dce38b02f778ac04fdc5b2
cambiph <incubusattax@gmail.com>
2019-09-25 14:38:39 +0200

    Parameterizeer pad naar componenten en mogelijkheid om naar major te updaten

--- didm/updateAllToLatestUtil.sh
+++ didm/updateAllToLatestUtil.sh
@@ -4,12 +4,14 @@ CWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 source $CWD/../utils/envUtils.sh
 source $CWD/../utils/gitUtils.sh
+source $CWD/../utils/npmUtils.sh
 
 checkIfRubyIsInstalled
 checkIfHomebrewIsInstalled
 checkIfHubIsInstalled
 
-read -e -p 'Waar bevinden zich de webcomponenten? (vb. /Users/philippe/localdev)' path
+read -e -p 'Waar bevinden zich de webcomponenten? (vb. /Users/philippe/localdev) ' path
+read -p "Mag vl-ui-util naar de laatste major worden geupdate? (Y/n) " choice
 
 WORKINGDIR=$path
 BRANCHNAME="maintenance"
@@ -21,18 +23,18 @@ for dir in $DIRS; do
     && echo "====================================================" \
     && echo " Working on $dir" \
     && echo "====================================================" \
-    && git master \
-    && git stash \
-    && git pull \
-    && checkIfMaintenanceBranchExists $BRANCHNAME \
-    && git checkout -b $BRANCHNAME \
-    && npm update vl-ui-util \
-    && npm install \
-    && git add -A \
-    && git commit -m "Automatic commit via update script" \
-    && git push --set-upstream origin $BRANCHNAME \
-    && hub pull-request --assign coemans --labels maintenance --no-edit \
-    && echo "====================================================" \
+    && git master
+#    && git stash \
+#    && git pull \
+#    && checkIfMaintenanceBranchExists $BRANCHNAME \
+#    && git checkout -b $BRANCHNAME \
+    checkBreaking $choice 
+#    && npm install \ 
+#    && git add -A \
+#    && git commit -m "Update vl-ui-util" 
+#    && git push --set-upstream origin $BRANCHNAME \
+#    && hub pull-request --assign coemans --labels maintenance --no-edit \
+    echo "====================================================" \
     && echo " Done for $dir" \
     && echo "====================================================" \
     && popd
